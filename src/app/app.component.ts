b7d6866eb2c6eee255c1259b252e1a889b11040b
PabloOmega <pablonavarretectdb1c@gmail.com>
2024-09-03 09:00:34 -0500

    modified:   .gitignore 	modified:   angular.json 	new file:   firebase.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.config.ts 	modified:   src/app/app.routes.ts 	new file:   src/app/icons-provider.ts 	new file:   src/app/pages/welcome/welcome.component.css 	new file:   src/app/pages/welcome/welcome.component.html 	new file:   src/app/pages/welcome/welcome.component.ts 	new file:   src/app/pages/welcome/welcome.routes.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterLink, 
+    RouterOutlet, 
+    NzIconModule, 
+    NzLayoutModule, 
+    NzMenuModule,
+    NzBreadCrumbModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-ngzorro-mat';
+  isCollapsed = false;
 }
