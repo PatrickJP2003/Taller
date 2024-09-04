3f2fd8c254d41186cc798284aa65db6baf66bbfc
PabloOmega <pablonavarretectdb1c@gmail.com>
2024-09-04 10:35:15 -0500

    modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.routes.ts 	new file:   src/app/pages/login/login.component.css 	new file:   src/app/pages/login/login.component.html 	new file:   src/app/pages/login/login.component.spec.ts 	new file:   src/app/pages/login/login.component.ts 	new file:   src/app/pages/register/register.component.css 	new file:   src/app/pages/register/register.component.html 	new file:   src/app/pages/register/register.component.spec.ts 	new file:   src/app/pages/register/register.component.ts 	new file:   src/app/services/registers/registers.service.spec.ts 	new file:   src/app/services/registers/registers.service.ts 	new file:   src/app/services/users/users.service.spec.ts 	new file:   src/app/services/users/users.service.ts

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, ValidatorFn, AbstractControl } from '@angular/forms';
+import { RegistersService } from '../../services/registers/registers.service';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { RouterLink } from '@angular/router';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    NzCheckboxModule,
+    ReactiveFormsModule,
+    NzIconModule,
+    RouterLink,
+    NzSelectModule,
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private registersService: RegistersService) {
+    this.form = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]],
+      confirmPassword: ["", [this.confirmationValidator]],
+      nickname: ["", [Validators.required]],
+      phoneNumber: ["", [Validators.required]],
+      agree: [false],
+      photoURL: [""],
+      role: ["Empleado"]
+    });
+  }
+
+  updateConfirmValidator(): void {
+    Promise.resolve().then(() => this.form.controls['confirmPassword'].updateValueAndValidity());
+  }
+
+  confirmationValidator: ValidatorFn = (control: AbstractControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.form.controls['password'].value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };  
+
+  onClickRegister(): void {
+    if(this.form.invalid) return;
+    this.registersService.createRegister(this.form.value, this.form.value)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  }
+
+  onClickRegisterGoogle(): void {
+    this.registersService.createRegisterWithGoogle()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  }
+
+}
